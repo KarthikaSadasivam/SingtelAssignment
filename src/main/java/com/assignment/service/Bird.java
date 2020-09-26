806953edb701a80d9d5652f73e3953d923fb4e7e
KarthikaSadasivam <nivkarthika@gmail.com>
2020-09-26 18:35:34 +0800

    Part A Answers

--- src/main/java/com/assignment/service/Bird.java
+++ src/main/java/com/assignment/service/Bird.java
@@ -1,5 +1,10 @@
 package com.assignment.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.assignment.utils.AnimalCharacters;
+
 public class Bird extends Animal {
 	
 	@Override
@@ -13,6 +18,47 @@ public class Bird extends Animal {
 	
 	public void sing(){
 		System.out.println("I am singing");
+	
+	}
+	
+	
+	public static void main(String args[]){
+		//Second Question Answer
+		System.out.println("Duck Says-->"+AnimalCharacters.doGetSound("duck"));
+		System.out.println("Duck Says-->"+AnimalCharacters.doGetAction("duck"));
+		System.out.println("Chicken Says-->"+AnimalCharacters.doGetSound("chicken"));
+		System.out.println("Chicken Says-->"+AnimalCharacters.doGetAction("chicken"));
+		
+		
+		getRoosterBehaviour();
+		getParrotChangedBehaviour();
+		
+		
+	}
+	//Modelling a Rooster
+	public static void getRoosterBehaviour(){
+		//Third Question Answer(Without Inheritance Concept)
+				System.out.println("Rooster Says-->"+AnimalCharacters.doGetSound("rooster"));
+				
+				List<String> doGetRelationList=new ArrayList<String>();
+				doGetRelationList=AnimalCharacters.getRelationShip("chicken", "rooster");
+				
+				doGetRelationList.forEach(animal->
+				{
+					System.out.println("Chicken and Rooster Has "+animal);
+				});
+	}
+	
+	//Modeling a Parrot
+	public static void getParrotChangedBehaviour(){
+		System.out.println("A parrot living with dogs says:"+AnimalCharacters.doGetSound("dog"));
+		System.out.println("A parrot living with cat says:"+AnimalCharacters.doGetSound("cat"));
+		System.out.println("A parrot living near the rooster says: "+AnimalCharacters.doGetSound("rooster"));
+		/*The parrot object is already maintanable as,
+		We can inherit the character of the another animal which it lives with gives desired output*/
+		System.out.println("A parrot living with Duck says:"+AnimalCharacters.doGetSound("duck"));
+		System.out.println("A parrot living near the Phone says: "+AnimalCharacters.doGetSound("phone"));
+		   
 	}
 	
 	
---
+++ src/main/java/com/assignment/utils/AnimalCharacters.java
@@ -0,0 +1,115 @@
+package com.assignment.utils;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public enum AnimalCharacters {
+	   
+	duck("Quack ,Quack" ,"Swim","duck","bird"),
+    chicken("Cluck, cluck","Walk","chicken","bird"),	 
+	rooster("Cock-a-doodle-doo","Walk","rooster","bird"),
+	parrot("Chip Chip","Fly","parrot","bird"),
+	dog("Woof, woof","walk","dog","animal"),
+	cat("Meow","walk","cat","animal"),
+	phone("Tring tring","entertain","phone","device");
+			  
+    private final String sound;
+    private final String action;
+    private final String name;
+    private final String species;
+    
+    
+
+
+	private AnimalCharacters(String sound,String action,String name,String species) {
+		this.sound = sound;
+		this.action = action;
+		this.name=name;
+		this.species=species;
+		
+	}
+ 
+	public static String doGetSound(String name)
+	{
+		String sounds="";
+		for(AnimalCharacters animal:AnimalCharacters.values())
+		{
+			if(animal.getName().equals(name))
+			{
+				sounds=animal.getSound();
+				break;
+			}
+		}
+		return sounds;
+	}
+
+	public static String doGetAction(String name)
+	{
+		String action="";
+		for(AnimalCharacters animal:AnimalCharacters.values())
+		{
+			if(animal.getName().equals(name))
+			{
+				action=animal.getAction();
+				break;
+			}
+		}
+		return action;
+	}
+	
+	public static String doGetSpecies(String name)
+	{
+		String species="";
+		for(AnimalCharacters animal:AnimalCharacters.values())
+		{
+			if(animal.getName().equals(name))
+			{
+				species=animal.getSpecies();
+				break;
+			}
+		}
+		return species;
+	}
+	
+	public static List<String> getRelationShip(String name1,String name2)
+	{
+		List<String> relationship=new ArrayList<String>();
+		
+			if(doGetSound(name1).equalsIgnoreCase(doGetSound(name2))){
+				relationship.add("Sound");
+			}
+			if(doGetAction(name1).equalsIgnoreCase(doGetAction(name2))){
+				relationship.add("Movement Behaviour");
+			}
+			if(doGetSpecies(name1).equalsIgnoreCase(doGetSpecies(name2))){
+				relationship.add("Species");
+			}
+			
+		System.out.println("relationship->"+relationship.size());
+		return relationship;
+	}
+	
+	
+	
+
+	public String getSound() {
+		return sound;
+	}
+
+	public String getAction() {
+		return action;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public String getSpecies() {
+		return species;
+	}
+
+	
+
+	
+	
+}
